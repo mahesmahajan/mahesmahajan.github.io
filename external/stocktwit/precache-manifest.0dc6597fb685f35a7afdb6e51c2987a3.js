2450aa29a40099d4096b829e4ce41015d8c50892
User <user@user.com>
2020-04-28 00:55:19 +0530

    Stcok Twi

---
+++ external/stocktwit/precache-manifest.0dc6597fb685f35a7afdb6e51c2987a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca23f5f52007e53db3b6ec6279079df",
+    "url": "/stocktwits-api-project/index.html"
+  },
+  {
+    "revision": "0e66922d069b46aceef7",
+    "url": "/stocktwits-api-project/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d9c8b743042caf812eb8",
+    "url": "/stocktwits-api-project/static/css/main.d1a19664.chunk.css"
+  },
+  {
+    "revision": "0e66922d069b46aceef7",
+    "url": "/stocktwits-api-project/static/js/2.03b97faa.chunk.js"
+  },
+  {
+    "revision": "d9c8b743042caf812eb8",
+    "url": "/stocktwits-api-project/static/js/main.66766d14.chunk.js"
+  },
+  {
+    "revision": "962f5928c78c219a9fc6",
+    "url": "/stocktwits-api-project/static/js/runtime-main.972a9c2b.js"
+  }
+]);
